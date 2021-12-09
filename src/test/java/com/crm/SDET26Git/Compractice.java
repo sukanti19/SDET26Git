31c2b1e85586ef0d2fb107227fe4de4b71ed2157
Lenovo <Lenovo@DESKTOP-BFRHE07>
2021-12-09 13:48:16 +0530

    Test demo created

---
+++ src/test/java/com/crm/SDET26Git/Compractice.java
@@ -0,0 +1,10 @@
+package com.crm.SDET26Git;
+
+import org.testng.annotations.Test;
+
+public class Compractice {
+	@Test
+	public void CompracticeDemo() {
+		System.out.println("Demo Script");
+	}
+}
