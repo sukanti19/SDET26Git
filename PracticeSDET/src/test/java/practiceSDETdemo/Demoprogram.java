d194e4dab5c79c80e3b1d08553f1839badaaddd8
Lenovo <Lenovo@192.168.120.48>
2021-12-10 06:29:48 +0530

    changes final

--- PracticeSDET/src/test/java/practiceSDETdemo/Demoprogram.java
+++ PracticeSDET/src/test/java/practiceSDETdemo/Demoprogram.java
@@ -8,6 +8,7 @@ public class Demoprogram {
 		System.out.println("1st statement");
 		System.out.println("2st statement");
 		System.out.println("3st statement");
+		System.out.println("3st statement");
 
 
 	}
